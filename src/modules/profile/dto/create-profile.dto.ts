6b39d98e6797d5bd3baecd62eb580c7ae817d542
kalekseychuk@innovorg.com <kalekseychuk@innovorg.com>
2022-12-09 22:11:29 +0700

    feat: profile - set optional props in dto

--- src/modules/profile/dto/create-profile.dto.ts
+++ src/modules/profile/dto/create-profile.dto.ts
@@ -1,27 +1,38 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsDateString, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import {
+  IsDateString,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 import { SexEnum } from '../profile.types';
 
 export class CreateProfileDto {
   @ApiProperty({ example: 'Олег', description: 'Имя пользователя' })
   @IsString({ message: 'Должно быть строкой' })
-  readonly firstName: string;
+  @IsOptional()
+  readonly firstName?: string;
 
   @ApiProperty({ example: 'Романцев', description: 'Фамилия пользователя' })
   @IsString({ message: 'Должно быть строкой' })
-  readonly lastName: string;
+  @IsOptional()
+  readonly lastName?: string;
 
   @ApiProperty({ example: 'Иванович', description: 'Отчество пользователя' })
   @IsString({ message: 'Должно быть строкой' })
-  readonly middleName: string;
+  @IsOptional()
+  readonly middleName?: string;
 
   @ApiProperty({ example: '04.01.1954', description: 'Дата рождения' })
   @IsDateString()
-  readonly birthDate: string;
+  @IsOptional()
+  readonly birthDate?: string;
 
   @ApiProperty({ example: '1', description: 'Пол. 1 - мужской, 2 - женский' })
   @IsNumber({})
-  readonly sex: SexEnum;
+  @IsOptional()
+  readonly sex?: SexEnum;
 
   @ApiProperty({ example: '1', description: 'Id пользователя-прототипа' })
   @IsNumber()
