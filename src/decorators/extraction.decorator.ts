41dfdc11846b6fa4a2284a537530ab81b5e7d303
WhiteBearDeveloper <viper010790@mail.ru>
2023-04-18 18:55:08 +0700

    feat: decorators - added zero value returning in user id extraction

--- src/decorators/extraction.decorator.ts
+++ src/decorators/extraction.decorator.ts
@@ -1,8 +1,8 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
 
 export const UserIdExtraction = createParamDecorator(
-  (data: unknown, req: ExecutionContext) => {
+  (data: unknown, req: ExecutionContext): number => {
     const request = req.switchToHttp().getRequest();
-    return request.user.id;
+    return request.user?.id ? request.user.id : 0;
   },
 );
